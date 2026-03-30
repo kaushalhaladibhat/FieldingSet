ec04a124f9fc45afbfe349f65e0666280a3f19e4
kaushalhaladibhat <kaushalhaladi2011@gmail.com>
2026-03-30 22:25:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TM_CONFIG = {
-  geminiKey: "AIzaSyA2xadXtxqW1qe6zTIa4xJNiJChjqi0nMQ",
+  geminiKey: "AIzaSyA7bnwz3L8JkRijmFJqgUeLvtZ4e1Las-0",
   vapidKey: "BMT-EaZtNroZ5fF_Eitxq5Le4ZewSCxkxhNGJwwOhmo4fTrRbIv0V2rltEZvv-leeIwinM6PmiV069srU7avy8o",
   firebaseConfig: {
     apiKey: "AIzaSyBg4lX_8ZMKSjdbzdV5dI1_dZOipJ56MK0",
